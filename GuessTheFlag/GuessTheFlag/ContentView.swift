78716908f101f1a41006eed19844fa694018922d
Tiger <tigeryyang@gmail.com>
2021-08-30 22:53:15 -0500

    Add part of day 34 challenge
    - TODO wrong flag animations

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -15,6 +15,10 @@ struct ContentView: View {
     @State private var countries: [String] = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var randomCountry: Int = Int.random(in: 0 ..< 3)
     
+    // animation related states
+    @State private var flagAnimationAmount: [Double] = [0.0, 0.0, 0.0]
+    @State private var flagOpacityAmount: [Double] = [1.0, 1.0, 1.0]
+    
     var body: some View {
         ZStack {
             LinearGradient(
@@ -40,6 +44,11 @@ struct ContentView: View {
                         }) {
                             FlagView(image: Image(self.countries[number]))
                         }
+                        .rotation3DEffect(
+                            .degrees(flagAnimationAmount[number]),
+                            axis: /*@START_MENU_TOKEN@*/(x: 0.0, y: 1.0, z: 0.0)/*@END_MENU_TOKEN@*/
+                        )
+                        .opacity(self.flagOpacityAmount[number])
                     }
                     // spacer pushes everything up towards the top
                     Spacer()
@@ -64,6 +73,22 @@ struct ContentView: View {
         if number == randomCountry {
             scoreTitle = "Correct, great job!"
             score += 1
+            
+            // for correct flag, triggered 360 spin
+            withAnimation(.easeInOut(duration: 1)) {
+                // trigger flag rotation animation
+                self.flagAnimationAmount[number] += 360
+            }
+            
+            // for other 2 flags, fade to 0% opacity
+            for index in 0..<3 {
+                if index != number {
+                    withAnimation(.easeOut(duration: 2)) {
+                        self.flagOpacityAmount[index] = 0.25
+                    }
+                }
+            }
+            
         } else {
             scoreTitle = """
                 Oops, wrong flag 🥲
@@ -80,6 +105,9 @@ struct ContentView: View {
         // shuffle list and regenerate random number
         countries.shuffle()
         randomCountry = Int.random(in: 0 ..< 3)
+        
+        // reset opacities
+        flagOpacityAmount = [1.0, 1.0, 1.0]
     }
 }
 
