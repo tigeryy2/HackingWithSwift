6a497ed54940ea12d2e706a21677f99de7d11797
Tiger <tigeryyang@gmail.com>
2021-09-12 23:19:57 -0500

    Complete day 51

--- CupcakeCorner/CupcakeCorner/CheckoutView.swift
+++ CupcakeCorner/CupcakeCorner/CheckoutView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct CheckoutView: View {
     @ObservedObject var order: Order
     
+    @State private var confirmationAlertMessage = ""
+    @State private var showingConfirmationAlert = false
+    
     var body: some View {
         GeometryReader { geometry in
             ScrollView {
@@ -23,14 +26,51 @@ struct CheckoutView: View {
                         .font(.title)
                     
                     Button(action: {
-                        // something here..
+                        self.placeOrder()
                     }) {
                         Text("Place Order")
+                            .font(.title2)
                     }
+                    .padding()
                 }
             }
         }
         .navigationBarTitle("Checkout", displayMode: .inline)
+        .alert(isPresented: $showingConfirmationAlert) {
+            Alert(title: Text("Thank you!"), message: Text(confirmationAlertMessage), dismissButton: .default(Text("OK")))
+        }
+    }
+    
+    func placeOrder() {
+        // encode the order object
+        guard let encoded = try? JSONEncoder().encode(self.order) else {
+            print("Failed to encode order")
+            return
+        }
+        
+        // prepared urlrequest in order to send encoded data to server
+        let url = URL(string: "https://reqres.in/api/cupcakes")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        request.httpBody = encoded
+        
+        // trigger data fetching in the background
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            // check that data is returned... if it is not, then we have errored
+            guard let data = data else {
+                print("No data in response: \(error?.localizedDescription ?? "Unknown error").")
+                return
+            }
+            
+            if let decodedOrder = try? JSONDecoder().decode(Order.self, from: data) {
+                self.confirmationAlertMessage = "Your order for \(decodedOrder.quantity)x \(Order.types[decodedOrder.type].lowercased()) cupcakes is on its way!"
+                self.showingConfirmationAlert = true
+            } else {
+                print("Invalid response from server")
+            }
+            
+        }.resume()
     }
 }
 
--- CupcakeCorner/CupcakeCorner/Order.swift
+++ CupcakeCorner/CupcakeCorner/Order.swift
@@ -7,7 +7,12 @@
 
 import Foundation
 
-class Order: ObservableObject {
+class Order: ObservableObject, Codable {
+    // all the properties that need to be encoded/decoded
+    enum CodingKeys: CodingKey {
+        case type, quantity, specialRequestEnabled, extraFrosting, addSprinkles, name, streetAddress, city, zip
+    }
+    
     // types of cakes that can be ordered
     static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
     
@@ -54,6 +59,45 @@ class Order: ObservableObject {
         }
         
         return cost
+    }
+    
+    required init(from decoder: Decoder) throws {
+        // inits object, loading data using decoder
+        
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        self.type = try container.decode(Int.self, forKey: .type)
+        self.quantity = try container.decode(Int.self, forKey: .quantity)
+        
+        self.specialRequestEnabled = try container.decode(Bool.self, forKey: .specialRequestEnabled)
+        self.extraFrosting = try container.decode(Bool.self, forKey: .extraFrosting)
+        self.addSprinkles = try container.decode(Bool.self, forKey: .addSprinkles)
+        
+        self.name = try container.decode(String.self, forKey: .name)
+        self.streetAddress = try container.decode(String.self, forKey: .streetAddress)
+        self.city = try container.decode(String.self, forKey: .city)
+        self.zip = try container.decode(String.self, forKey: .zip)
+        
+    }
+    
+    init() {
+        // inits object w/ blank data
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        
+        try container.encode(type, forKey: .type)
+        try container.encode(quantity, forKey: .quantity)
+        
+        try container.encode(specialRequestEnabled, forKey: .specialRequestEnabled)
+        try container.encode(extraFrosting, forKey: .extraFrosting)
+        try container.encode(addSprinkles, forKey: .addSprinkles)
+        
+        try container.encode(name, forKey: .name)
+        try container.encode(streetAddress, forKey: .streetAddress)
+        try container.encode(city, forKey: .city)
+        try container.encode(zip, forKey: .zip)
     }
     
 }
