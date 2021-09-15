47cb1dd90ae87286b2fe15e849f0f1b24aeca85a
Tiger <tigeryyang@gmail.com>
2021-09-15 16:18:34 -0500

    Complete day 57

---
+++ CoreDataProject/CoreDataProject/Movie+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Movie+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Tiger Yang on 9/14/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Movie)
+public class Movie: NSManagedObject {
+
+}
