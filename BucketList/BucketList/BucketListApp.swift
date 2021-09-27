ba6d86d6872603ed0626e30db88e364851e80e7f
Tiger <tigeryyang@gmail.com>
2021-09-26 20:44:39 -0500

    Complete day 68

---
+++ BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Tiger Yang on 9/26/21.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
