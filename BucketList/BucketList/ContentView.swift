04c885af142f2794960017ec7620b856597bc99b
Tiger <tigeryyang@gmail.com>
2021-09-28 22:20:11 -0500

    Complete day 70

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -5,51 +5,60 @@
 //  Created by Tiger Yang on 9/26/21.
 //
 
-import LocalAuthentication
+import MapKit
 import SwiftUI
 
 struct ContentView: View {
-    @State private var isUnlocked = false
-    
-    func authenticate() {
-        let context = LAContext()
-        var error: NSError?
-
-        // check whether biometric authentication is possible
-        if (context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error)) {
-            // provide reason we're asking to sue biometrics
-            let reason = "We need to unlock your data."
-
-            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
-                // authentication has now completed
-                DispatchQueue.main.async {
-                    if success {
-                        // authenticated successfully
-                        self.isUnlocked = true
-                    } else {
-                        // there was a problem
-                    }
-                }
-            }
-        } else {
-            // no biometrics
-        }
-    }
-
+    @State private var centerCoordinate = CLLocationCoordinate2D()
+    @State private var places = [MKPointAnnotation]()
+    @State private var selectedPlace: MKPointAnnotation?
+    @State private var showingPlaceDetails = false
     
     var body: some View {
-        VStack {
-            if self.isUnlocked {
-                Text("Unlocked")
-            } else {
-                Text("Locked")
-            }
-            
-            MapView()
+        ZStack {
+            MapView(
+                centerCoordinate: self.$centerCoordinate,
+                selectedPlace: self.$selectedPlace,
+                showingPlaceDetails: self.$showingPlaceDetails,
+                annotations: self.places)
                 .edgesIgnoringSafeArea(.all)
+            Circle()
+                .fill(Color.blue)
+                .opacity(0.25)
+                .frame(width: 32, height: 32)
+            // Place button in buttom right corner
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        // add current coord to list of places
+                        let newPlace = MKPointAnnotation()
+                        newPlace.coordinate = self.centerCoordinate
+                        newPlace.title = "Example Location"
+                        self.places.append(newPlace)
+                    }) {
+                        Image(systemName: "plus")
+                    }
+                    .padding()
+                    .font(.title)
+                    .background(Color.black.opacity(0.59))
+                    .foregroundColor(.white)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke().foregroundColor(Color.white.opacity(0.75)))
+                    .padding(.trailing)
+                }
+            }
         }
-        .onAppear(perform: authenticate)
-        
+        .alert(isPresented: self.$showingPlaceDetails) {
+            Alert(
+                title: Text(self.selectedPlace?.title ?? "Unknown"),
+                message: Text(self.selectedPlace?.subtitle ?? "Missing place information."),
+                primaryButton: .default(Text("OK")),
+                secondaryButton: .default(Text("Edit")) {
+                // edit this place
+            })
+        }
     }
 }
 
--- BucketList/BucketList/MapView.swift
+++ BucketList/BucketList/MapView.swift
@@ -5,10 +5,15 @@
 //  Created by Tiger Yang on 9/27/21.
 //
 
-import SwiftUI
 import MapKit
+import SwiftUI
 
 struct MapView: UIViewRepresentable {
+    @Binding var centerCoordinate: CLLocationCoordinate2D
+    @Binding var selectedPlace: MKPointAnnotation?
+    @Binding var showingPlaceDetails: Bool
+    var annotations: [MKPointAnnotation]
+    
     // notified and takes control when something "interesting" happens
     class Coordinator: NSObject, MKMapViewDelegate {
         var parent: MapView
@@ -17,18 +22,49 @@ struct MapView: UIViewRepresentable {
             self.parent = parent
         }
         
+        // funcs with particular signatures are looked for here, to decide which events notify and trigger actions
+        
+        // fetch view for annotation, automatically called by swift when needed
         func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-            let view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: nil)
-            view.canShowCallout = true
-            return view
+            // this is our unique identifier for view reuse
+            let identifier = "Placemark"
+            
+            // attempt to find a cell we can recycle
+            var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+            
+            if annotationView == nil {
+                // we didn't find one; make a new one
+                annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+                
+                // allow this to show pop up information
+                annotationView?.canShowCallout = true
+                
+                // attach an information button to the view
+                annotationView?.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+            } else {
+                // we found a view to reuse, so give it the new annotation
+                annotationView?.annotation = annotation
+            }
+            
+            // whether it's a new view or a recycled one, send it back
+            return annotationView
         }
         
-        // funcs with particular signatures are looked for here, to decide which events notify and trigger actions
+        // gets called when the "i" button on the annotation view gets pressed
+        func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+            // check that we have valid annotation
+            guard let placemark = view.annotation as? MKPointAnnotation else { return }
+            
+            // pass the clicked place to the parent, and trigger alert
+            parent.selectedPlace = placemark
+            parent.showingPlaceDetails = true
+        }
         
         // automatically called whenever visible region is changed..
         // e.g. zoomed, dragged to new area...
         func mapViewDidChangeVisibleRegion(_ mapView: MKMapView) {
-            print(mapView.centerCoordinate)
+            // pass new center coord to the parent's binding
+            parent.centerCoordinate = mapView.centerCoordinate
         }
         
     }
@@ -38,26 +74,41 @@ struct MapView: UIViewRepresentable {
         Coordinator(self)
     }
     
-    func makeUIView(context: UIViewRepresentableContext<MapView>) -> MKMapView {
+    // use type alias "Context" instead of "UIViewRepresentableContext<MapView>"
+    func makeUIView(context: Context) -> MKMapView {
         let mapView = MKMapView()
         mapView.delegate = context.coordinator
-        
-        // add map annotation for London
-        let annotation = MKPointAnnotation()
-        annotation.title = "London"
-        annotation.subtitle = "Capital of England"
-        annotation.coordinate = CLLocationCoordinate2D(latitude: 51.5, longitude: 0.13)
-        mapView.addAnnotation(annotation)
         
         return mapView
     }
     
-    func updateUIView(_ view: MKMapView, context: UIViewRepresentableContext<MapView>) {
+    func updateUIView(_ view: MKMapView, context: Context) {
+        // compares changed list of annotations to current list of annotations, if number is different, then we've added or removed an annotation
+        if annotations.count != view.annotations.count {
+            view.removeAnnotations(view.annotations)
+            view.addAnnotations(annotations)
+        }
     }
 }
 
 struct MapView_Previews: PreviewProvider {
+    
     static var previews: some View {
-        MapView()
+        MapView(
+            centerCoordinate: .constant(MKPointAnnotation.example.coordinate),
+            selectedPlace: .constant(MKPointAnnotation.example),
+            showingPlaceDetails: .constant(false),
+            annotations: [MKPointAnnotation.example])
+    }
+}
+
+// add some sample data for the preview
+extension MKPointAnnotation {
+    static var example: MKPointAnnotation {
+        let annotation = MKPointAnnotation()
+        annotation.title = "London"
+        annotation.subtitle = "Home to the 2012 Summer Olympics."
+        annotation.coordinate = CLLocationCoordinate2D(latitude: 51.5, longitude: -0.13)
+        return annotation
     }
 }
